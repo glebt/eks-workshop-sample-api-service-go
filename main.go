bdfcc4c1b0deaf5c82d334d774ec7854cb48b095
Gleb Tulukin <glebtulukin@gmail.com>
2019-06-17 12:08:51 -0400

    getting ready for demo

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "CHANGE ME!!!"}
+		res := &response{Message: "Germantown"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
